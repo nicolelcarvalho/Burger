54a33ac623d1f7eb77edd089a1d2a8448ed4a9e0
Nicole Carvalho <NicoleCarvalho@Nicole-Carvalhos-MacBook-Pro.local>
2017-10-12 16:04:28 -0400

    uploading files

---
+++ public/assets/js/burger.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function() { 
+	$(".devour").on("click", function() {
+		var id = $(this).data("burgerid");
+
+		var updatedBurger = {
+			devoured: true
+		};
+
+		$.ajax("/api/burgers/" + id, {
+			type: "PUT",
+			data: updatedBurger
+		}).then(function() {
+			console.log("updated id ", id);
+			location.reload();
+			});
+	});
+});
