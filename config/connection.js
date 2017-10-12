059ec382fe6e40b6b8c1320b1a078f45c08880a4
Nicole Carvalho <NicoleCarvalho@Nicole-Carvalhos-MacBook-Pro.local>
2017-10-12 16:26:18 -0400

    uploading to heroku, changed connection to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,25 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "burgers_db"
+// });
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: '',
+		database: 'burgers_db'
+	});
+};
+
 
 // Make connection.
 connection.connect(function(err) {
